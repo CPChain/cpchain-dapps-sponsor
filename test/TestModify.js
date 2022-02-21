4baf425f538c292b2734936980dadc2ed13438ec
blade_ly <blade_ly@aliyun.com>
2022-02-21 14:52:39 +0800

    test function modifyDapp

--- test/TestModify.js
+++ test/TestModify.js
@@ -22,45 +22,72 @@ contract("Sponsor modifyDapp", (accounts) => {
     it("2 dapp is not exist", async() => {
         instance = await Sponsor.deployed()
         try {
-            await instance.modifyDapp(2,"test_3",accounts[1], "www.test.2.com", accounts[2], "{}", {from: accounts[1]})
+            await instance.modifyDapp(2,"test_3", accounts[1], "www.test.2.com", accounts[2], "{}", {from: accounts[1]})
             assert.fail()
         }
         catch(error) {
             assert.ok(error.toString().includes("This dapp is not registered"))
         }
     })
-    // it("3 dapp is deregisted", async() => {
-    //     instance = await Sponsor.deployed()
-    //     await instance.registerDapp("test_3", accounts[1], "www.test.3.com", accounts[2], "{}", {from: accounts[1]})
-    //     await instance.deregisterDapp(2, {from: accounts[1]})
-    //     try {
-    //         await instance.modifyDapp(2,"test_3",accounts[1], "www.test.3.com", accounts[2], "{}", {from: accounts[1]})
-    //         assert.fail()
-    //     }
-    //     catch(error) {
-    //         assert.ok(error.toString().includes("This dapp is deregisted"))
-    //     }
-    // })
-    // it("3 id = 3, Dapps[3] is deregistered", async() => {
-    //     instance = await Sponsor.deployed()
-    //     await instance.registerDapp("test_3", accounts[1], "www.test.3.com", accounts[2], "{}", {from: accounts[1]})
-    //     await instance.deregisterDapp(3, {from: accounts[1]})
-    //     try {
-    //         await instance.deregisterDapp(3, {from: accounts[1]})
-    //         assert.fail()
-    //     }
-    //     catch(error) {
-    //         assert.ok(error.toString().includes("This dapp is deregistered"))
-    //     }
-    // })
-    // it("3 id = 4, Dapps[4] is not exist", async() => {
-    //     instance = await Sponsor.deployed()
-    //     try {
-    //         await instance.deregisterDapp(4, {from: accounts[1]})
-    //         assert.fail()
-    //     }
-    //     catch(error) {
-    //         assert.ok(error.toString().includes("You're not the owner of this dapp"))
-    //     }
-    // })
+    it("3 dapp is deregisted", async() => {
+        instance = await Sponsor.deployed()
+        await instance.registerDapp("test_4", accounts[1], "www.test.3.com", accounts[2], "{}", {from: accounts[1]})
+        await instance.deregisterDapp(2, {from: accounts[1]})
+        try {
+            await instance.modifyDapp(2,"test_5", accounts[1], "www.test.3.com", accounts[2], "{}", {from: accounts[1]})
+            assert.fail()
+        }
+        catch(error) {
+            assert.ok(error.toString().includes("This dapp is deregistered"))
+        }
+    })
+    it("4 sender != Dapp[id].registrant", async() => {
+        instance = await Sponsor.deployed()
+        try {
+            await instance.modifyDapp(1,"test_6", accounts[1], "www.test.6.com", accounts[2], "{}", {from: accounts[2]})
+            assert.fail()
+        }
+        catch(error) {
+            assert.ok(error.toString().includes("You're not the owner of this dapp"))
+        }
+    })
+    it("5 name is already been used", async() => {
+        instance = await Sponsor.deployed()
+        await instance.registerDapp("test_4", accounts[1], "www.test.3.com", accounts[2], "{}", {from: accounts[1]})
+        try {
+            await instance.modifyDapp(1,"test_4", accounts[1], "www.test.6.com", accounts[2], "{}", {from: accounts[1]})
+            assert.fail()
+        }
+        catch(error) {
+            assert.ok(error.toString().includes("This name has already been used"))
+        }
+    })
+    it("6 length of name,url,extendedinfo greater than max", async() => {
+        instance = await Sponsor.deployed()
+        const str = "a"
+        const name = str.repeat(101)
+        const url = str.repeat(101)
+        const info = str.repeat(201)
+        try {
+            await instance.modifyDapp(1, name, accounts[1], "www.test.6.com", accounts[2], "{}", {from: accounts[1]})
+            assert.fail()
+        }
+        catch(error) {
+            assert.ok(error.toString().includes("Length of name should less than 100"))
+        }
+        try {
+            await instance.modifyDapp(1, "test_2", accounts[1], url, accounts[2], "{}", {from: accounts[1]})
+            assert.fail()
+        }
+        catch(error) {
+            assert.ok(error.toString().includes("Length of url should less than 100"))
+        }
+        try {
+            await instance.modifyDapp(1, "test_2", accounts[1], "www.test.6.com", accounts[2], info, {from: accounts[1]})
+            assert.fail()
+        }
+        catch(error) {
+            assert.ok(error.toString().includes("Length of extendedInfo should less than 200"))
+        }
+    })
 })
